bb040a86cea3175706af7899f45b5f6499e3d441
trieutruong <trieutruong.dev@gmail.com>
2026-04-30 21:46:13 +0700

    feat: add SYNAPSE_API_URL support for local testing

--- app/lib/synapse.server.ts
+++ app/lib/synapse.server.ts
@@ -1,6 +1,6 @@
 import { Synapse } from '@pyrx/synapse';
 
-export const synapse = new Synapse({
+export const synapse = new Synapse({ baseUrl: process.env.SYNAPSE_API_URL || "https://synapse-api.pyrx.tech",
   apiKey: process.env.SYNAPSE_API_KEY!,
   workspaceId: process.env.SYNAPSE_WORKSPACE_ID!,
 });
